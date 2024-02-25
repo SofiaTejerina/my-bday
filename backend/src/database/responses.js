74cd21233ea1d809405b42d30c3dd3ccee1f7e6f
Sofia Tejerina <sofiatejerina23@gmail.com>
2024-02-25 16:16:25 -0300

    add docker compose to all services

--- backend/src/database/responses.js
+++ backend/src/database/responses.js
@@ -3,23 +3,35 @@ import sql from './sqlClient.js'
 class ResponsesDB {
 
     async getResponses() {
-      const responses = await sql`
+        await this.validateTableExist()
+        const responses = await sql`
         select
-          *
+            *
         from responses
-      `;
-      return responses;
+        `;
+        return responses;
     }
     
     async insertResponse({selectedGames = [], name="", threeWords="", flavours=""}) {
-      const responses = await sql`
+        await this.validateTableExist()
+        const responses = await sql`
         insert into responses
-          (selectedGames, name, threeWords, flavours)
+            (selectedGames, name, threeWords, flavours)
         values
-          (${ selectedGames }, ${ name }, ${ threeWords }, ${ flavours })
+            (${ selectedGames }, ${ name }, ${ threeWords }, ${ flavours })
         returning selectedGames, name, threeWords, flavours, id
-      `
-      return responses
+        `
+        return responses
+    }
+
+    async validateTableExist() {
+        await sql`create table if not exists responses (
+            selectedGames VARCHAR[],
+            name VARCHAR,
+            threeWords VARCHAR,
+            flavours VARCHAR,
+            id SERIAL PRIMARY KEY
+        )`
     }
 }
 
